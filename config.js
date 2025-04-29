46fe2ba97727303d18b09ea050648e645b452e52
OleksandrUA <124906757+Oleksandr231989@users.noreply.github.com>
2025-04-29 08:33:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-    powerBiUrl: "POWER_BI_URL",  // Placeholder, to be replaced securely
+    powerBiUrl: 'POWER_BI_URL', // Will be replaced by the secret value
     firebase: {
-        apiKey: "FIREBASE_API_KEY",  // Placeholder
-        authDomain: "FIREBASE_AUTH_DOMAIN",
-        projectId: "FIREBASE_PROJECT_ID",
-        storageBucket: "FIREBASE_STORAGE_BUCKET",
-        messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-        appId: "FIREBASE_APP_ID"
+        apiKey: 'FIREBASE_API_KEY',
+        authDomain: 'FIREBASE_AUTH_DOMAIN',
+        projectId: 'FIREBASE_PROJECT_ID',
+        storageBucket: 'FIREBASE_STORAGE_BUCKET',
+        messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+        appId: 'FIREBASE_APP_ID'
     }
 };
