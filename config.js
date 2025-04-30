c24291a9c686b777b9be12e77a247f0019674d19
OleksandrUA <124906757+Oleksandr231989@users.noreply.github.com>
2025-04-30 14:06:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,11 @@
-// Firebase and Power BI configuration
-// This file will be replaced during deployment with actual values
-// from GitHub secrets
-
 const config = {
-    powerBiUrl: 'POWER_BI_URL',
+    powerBiUrl: '##POWER_BI_URL##',
     firebase: {
-        apiKey: 'FIREBASE_API_KEY',
-        authDomain: 'FIREBASE_AUTH_DOMAIN',
-        projectId: 'FIREBASE_PROJECT_ID',
-        storageBucket: 'FIREBASE_STORAGE_BUCKET',
-        messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
-        appId: 'FIREBASE_APP_ID'
+        apiKey: '##FIREBASE_API_KEY##',
+        authDomain: '##FIREBASE_AUTH_DOMAIN##',
+        projectId: '##FIREBASE_PROJECT_ID##',
+        storageBucket: '##FIREBASE_STORAGE_BUCKET##',
+        messagingSenderId: '##FIREBASE_MESSAGING_SENDER_ID##',
+        appId: '##FIREBASE_APP_ID##'
     }
 };
-
-// Make configuration available to the page
-// Do not modify this line
-if (typeof module !== 'undefined') {
-    module.exports = config;
-}
